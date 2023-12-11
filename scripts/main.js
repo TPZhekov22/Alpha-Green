c66234a762af361d2e5098b667b0372e4964bbab
YVTomov22 <YVTomov22@codingburgas.bg>
2023-12-11 18:36:53 +0200

    Product page

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Get all paragraphs with the class "fade-in"
     var paragraphs = document.querySelectorAll(".fade-in");
 
-    // Function to add the fade-in animation class
     function fadeIn() {
         paragraphs.forEach(function(paragraph) {
             paragraph.classList.add("fade-in-animation");
         });
     }
 
-    // Trigger the fadeIn function after a short delay
-    setTimeout(fadeIn, 500); // Adjust the delay as needed
+    setTimeout(fadeIn, 500);
 });
